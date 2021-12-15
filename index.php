a1389081fdf2f5be180035de337b702f11ed80b3
Kaushik-Shetti-Kilowott <92842022+Kaushik-Shetti-Kilowott@users.noreply.github.com>
2021-12-15 15:09:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,16 @@ Enter Second Number:
 <?php  
     if(isset($_POST['submit']))  
     {  
-      
+       <script type="text/javascript">
+        window.location.href="http://localhost:5501/index.html"
+        </script>
         $number1 = $_POST['number1'];  
         $number2 = $_POST['number2'];  
         $sum =  $number1+$number2;     
 echo "The sum of $number1 and $number2 is: ".$sum;   
 
 }  
+
 ?>  
 <!-- </body>  
 </html>  -->
